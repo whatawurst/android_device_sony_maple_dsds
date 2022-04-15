fb9867048a975929840d0bade849ebe899a21e9a
derfelot <derfelot.git@gmail.com>
2022-04-15 22:03:23 +0200

    maple_dsds: Make maple great again
    Enable treble with full vendor partition and move to FBE:
      * enable uncompressed APEX
      * remove the need for prebuilt vndk28 libprotobuf lib's
      * enable vdnk current
      * don't build fde requirements anymore
      * don't build libandroid_net (now implied)
      * switch to regular yoshino fstab
      * remove adreno symlinks - not needed anymore
      * build vendor.img with size of oem partition (400MB)
      * drop manifests for cryptfshw (fde)
      * drop BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED (now implied)
      * switch to product_launched_with_o
      * re-work blob's list (move/copy things to /vendor)
      * kang gatekeeper and keymaster from poplar
      * bump ro.product.first_api_level to 26
      * some general cleanup's here and there

--- device/init.mk
+++ device/init.mk
@@ -2,7 +2,5 @@
 PRODUCT_PACKAGES += \
     init.recovery.maple_dsds \
     init.maple_dsds \
+    fstab.maple_dsds \
     ueventd.maple_dsds
-
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/ramdisk/fstab.maple_dsds:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.maple_dsds
